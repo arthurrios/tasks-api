e267ed9a675a65c83d70c0be7e727795be6dbbcf
arthur <arthur.rios007@gmail.com>
2024-09-01 11:34:02 -0300

    add regex for route params

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from "./database.js"
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "./utils/build-route-path.js"
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const tasks = database.select('tasks')
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const { title, description } = req.body
 
@@ -31,4 +32,11 @@ export const routes = [
       return res.writeHead(201).end()
     }
   },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      return res.end()
+    }
+  }
 ]
