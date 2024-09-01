dd09b19e3d4e3f06d3bbbdb855ffad2e33cb52a0
arthur <arthur.rios007@gmail.com>
2024-09-01 10:25:08 -0300

    add list and create methods with buffers

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import http from 'node:http'
+
+const tasks = []
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
+  if (method === 'GET' && url === '/tasks') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(tasks))
+  }
+
+  if (method === 'POST' && url === '/tasks') {
+    const { title, description } = req.body
+
+    tasks.push({
+      id: 1,
+      title,
+      description,
+      completed_at: null
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end();
+})
+
+server.listen(3333)
